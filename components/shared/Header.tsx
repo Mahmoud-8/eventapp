3f25460348fe480358c6e0986d6751398d8ebb0b
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2024-02-08 20:45:34 +0100

    finsh setup

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header className='w-full border-b'>
+        <div className='wrapper flex items-center justify-between'>
+            <Link href="/" className='w-36'> 
+            <Image src="/assets/images/logo.svg" width={128} height={38} alt='logo' />
+            </Link>
+            <div className='flex w-32 justify-end gap-3'>
+
+            </div>
+
+        </div>
+    </header>
   )
 }
 
