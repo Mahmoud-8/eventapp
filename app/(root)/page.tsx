75be1fe6a076f0b07546dfa4697c86994925d37a
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2024-02-08 19:35:57 +0100

    finsh setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,12 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-4x1"></h1>
-      <Button className="">
-        
-
-        Hello
-      </Button>
+      
       
     </main>
   );
